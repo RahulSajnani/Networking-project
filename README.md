5c3611d0578d5fe2cec5bda38969be3469a7415a
RahulSajnani <rahulsajnani2010@hotmail.com>
2020-03-26 01:48:48 +0530

    Updated README and changed server to accept connection from different device

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Networking-project
 Communication networks project for file uploading and downloading.
+
+__________________________________________________________________
+
+### Thing to do:
+
+[x] Connection between client and server.
+[ ] View files of designated folder.
+[ ] FileHashing
+[ ] IndexGet flag shortlist ad longlist
+[ ] FileDownload
+[ ] Caching
--- src/server.py
+++ src/server.py
@@ -9,14 +9,14 @@ if __name__ == "__main__":
     # AF_INET refers to the address family ipv4. The SOCK_STREAM means connection oriented TCP protocol.
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    host_ip = '127.0.0.1'
+    # host = socket.gethostname()
+    host = '0.0.0.0'
     port_number = 13000
-    server_socket.bind((host_ip, port_number))
-
+    server_socket.bind((host, port_number))
+    # Listen for clients
+    server_socket.listen(5)
+    
     while True:
-
-        # Listen for clients
-        server_socket.listen()
 
         # Accepting connection with client
         client_socket, client_ip  = server_socket.accept()
