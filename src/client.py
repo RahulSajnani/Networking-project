d02edb3d9ab6db6c2e07bc2d6bdf889226d6e37d
RahulSajnani <rahulsajnani2010@hotmail.com>
2020-04-02 22:39:42 +0530

    Update file hash for single file

--- src/client.py
+++ src/client.py
@@ -1,33 +1,66 @@
 import socket
 import config
 
+class Client:
+
+    def __init__(self):
+
+        self.client_socket = socket.socket()
+        self.host_ip = ''
+        self.port_number = 13000
+        self.connection = False
+
+    def authenticate(self):
+        
+        self.client_socket.connect((self.host_ip, self.port_number))
+        msg = self.client_socket.recv(1024)
+
+        # password = input(msg.decode('utf-8') + '\n')
+        password = config.PASSWORD
+        self.client_socket.send(password.encode('utf-8'))
+        reply = self.client_socket.recv(1024)
+
+        if reply.decode('utf-8') == '1':
+            print('Authenticated. Welcome!')
+            self.connection = True
+        else:
+            print('Incorrect password! Disconnecting')
+            self.connection = False
+
+        
+
+    def decode_command(self, command):
+
+        command_list = command.split(' ')
+        self.client_socket.send(command.encode('utf-8'))
+
+        if command_list[0] == 'FileHash':
+            
+            if command_list[1] == 'verify':    
+                reply = self.client_socket.recv(1024)
+                reply = reply.decode('utf-8')
+                print(command_list[2] + ' file hash: ' + reply)
+
+
+        elif command_list[0] == 'quit':
+            self.connection = False
+        pass
+    
+    def run(self):
+
+        # Authenticates client
+        self.authenticate()
+        
+        while self.connection:
+
+            command = input('Write command \n')
+            self.decode_command(command)
+            
+            
+        pass
+
+
 if __name__ == "__main__":
     
-    client_socket = socket.socket()
-    host_ip = ''
-    port_number = 13000
-    client_socket.connect((host_ip, port_number))
-    
-    msg = client_socket.recv(1024)
-    # password = input(msg.decode('utf-8') + '\n')
-    password = config.PASSWORD
-    client_socket.send(password.encode('utf-8'))
-    reply = client_socket.recv(1024)
-
-    if reply.decode('utf-8') == '1':
-        print('Authenticated. Welcome!')
-        connection = True
-    else:
-        print('Incorrect password! Disconnecting')
-        connection = False
-
-    while connection:
-    
-        command = input('Write command \n')
-        
-        client_socket.send(command.encode('utf-8'))
-        
-        if command == 'quit':
-            connection = False
-
-        
+    client = Client()
+    client.run() 
